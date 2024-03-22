83c811c47deac90835117cfeb73647457abcd4e9
Roger Correa <j.rogercorreamello@gmail.com>
2024-03-22 14:05:31 -0300

    commit

---
+++ Aula 4/estudante.cs
@@ -0,0 +1,11 @@
+// ./estudante.cs
+
+public class Estudante {
+    public string nome{get; set;}
+    public string matricula{get; set;}
+    public int idade{get; set;}
+
+    public void Matricular() {
+        
+    }
+}
---
+++ Aula 4/turma.cs
@@ -0,0 +1,13 @@
+// ./turma.cs
+
+public class Turma {
+    public int anoFormacao{get; set;}
+    public string turno {get; set;}
+    public string sala {get; set;}
+
+    public List<Estudante> estudantes = new List<Estudante>();
+
+    public void AdicionarEstudante(Estudante e) {
+        
+    }
+}
